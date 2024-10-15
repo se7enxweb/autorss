6ebaeb5c87e29f7ae91ee82c47cc53f7e2831d74
7x <info@se7enx.com>
2024-10-15 06:52:53 +0000

    Added: License, Documentation, Composer Support and Moved extension internals to repository root for composer based installations to begin

---
+++ ezinfo.php
@@ -0,0 +1,16 @@
+<?php
+
+class autorssInfo
+{
+    function info()
+    {
+        return array(
+            'Name' => '<a href="https://github.com/se7enxweb/autorss">Auto RSS</a>',
+            'Version' => "1.1",
+            'Copyright' => "Copyright (C) 2007 - 2008 Kristof Coomans and 1999 - 2024 7x",
+            'License' => "GNU General Public License v2.0"
+        );
+    }
+}
+
+?>
